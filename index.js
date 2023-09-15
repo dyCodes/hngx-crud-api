e9f230883b8412e7b0755037f3d1159ca56dbf49
dyCodes <yusufdaudu51@gmail.com>
2023-09-15 04:12:20 +0100

    feat: Implement CORS

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 const bodyParser = require("body-parser");
+require("dotenv/config");
 const {
 	getAll,
 	getPerson,
@@ -9,23 +11,20 @@ const {
 	updatePerson,
 	deletePerson,
 } = require("./controllers/person.controller");
-require("dotenv/config");
 
 // Connect to DB
 mongoose.connect(process.env.MONGODB_CONNECTION, { useNewUrlParser: true });
-// Check connection
 mongoose.connection.on("connected", () => console.log("Connected to database"));
 mongoose.connection.on("error", (err) => console.error(err));
 
-// Middleware
+// Middlewares
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Routes
-app.get("/", (req, res) => {
-	res.send("HNGx Stage two task [dyCodes]");
-});
-
+app.get("/", (req, res) => res.send("HNGx Stage two task [dyCodes]"));
+// Person routes
 app.get("/api", getAll);
 app.get("/api/:id", getPerson);
 app.post("/api", addPerson);
